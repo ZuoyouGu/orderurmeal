eed7340fdd3675970caac89baba23c24e738bf9f
Zuoyou Gu <guzuoyou.zju@gmail.com>
2013-09-08 15:53:33 -0400

    First try
    try to use github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orderurmeal::Application.config.secret_token = '89247620d3b0b7b1e8bfbd4a88f2733f395384acb5782373157e23f2394ed29cf0510236a71b48b7cc388bf242bea4169fcf2e1fd7d6adb4fcd7ba1c1a303a9a'
